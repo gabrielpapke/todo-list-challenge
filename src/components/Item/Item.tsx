20de158883b50befec2b01cc1cfedd480b9f913a
Gabriel Papke Paganini <gabrielpapkepaganini@192.168.0.8>
2023-04-24 21:22:37 -0300

    feat: list component

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,5 +1,27 @@
+import { Check, Trash } from "@phosphor-icons/react";
 import styles from "./Item.module.css";
 
 export function Item() {
-	return <div className={styles.item}>Item</div>;
+	return (
+		<div className={`${styles.item} ${styles.itemDone}`}>
+			<label className={styles.checkIcon}>
+				<input type="checkbox" />
+
+				<span className={styles.checked}>
+					<Check size={12} weight="bold" className="checked" />
+				</span>
+
+				<span className={styles.unchecked}></span>
+			</label>
+
+			<span className={styles.title}>
+				Integer urna interdum massa libero auctor neque turpis turpis semper.
+				Duis vel sed fames integer.
+			</span>
+
+			<span className={styles.removeIcon}>
+				<Trash size={20} />
+			</span>
+		</div>
+	);
 }
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,12 +1,37 @@
 import { Item } from "../Item/Item";
+
 import styles from "./List.module.css";
+import emptyIcon from "../../assets/empty.svg";
 
 export function List() {
 	return (
-		<div className={styles.list}>
-			<Item />
-			<Item />
-			<Item />
+		<div className={styles.wrapper}>
+			<div className={styles.header}>
+				<span>
+					Tarefas criadas
+					<span className={styles.badge}>5</span>
+				</span>
+
+				<span>
+					Concluídas
+					<span className={styles.badge}>2 de 5</span>
+				</span>
+			</div>
+
+			<div className={styles.empty}>
+				<img src={emptyIcon} className={styles.emptyIcon} />
+
+				<p>
+					<strong>Você ainda não tem tarefas cadastradas</strong>
+					Crie tarefas e organize seus itens a fazer
+				</p>
+			</div>
+
+			<div>
+				<Item />
+				<Item />
+				<Item />
+			</div>
 		</div>
 	);
 }
